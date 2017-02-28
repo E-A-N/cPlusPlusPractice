8829f47ff60cced74388b4992b71bcca891c7f9f
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-02-28 15:09:02 -0800

    playing around with classes *shrugs*

--- preProcessTest.cpp
+++ preProcessTest.cpp
@@ -6,26 +6,31 @@ using namespace std;
 #define tripler(x)(x+x+x)
 int const Monster = 55;
 
-int multiplyer(int root, int rep){
-  
-    if (rep == 0 || root == 0){
-       return 0;
-    }
-  
-    return root + multiplyer(root, rep - 1);
-}
 
+class featureMath {
+    public:
+        int multiplyer(int root, int rep){
+          
+            if (rep == 0 || root == 0){
+               return 0;
+            }
+          
+            return root + multiplyer(root, rep - 1);
+        }
+};
 
 int main(){
   
     int z = 7;
     int x = 19;
+    featureMath feature;
+    
     x = z == 5 ? doubler(x) : tripler(x);
     
     cout << "Testing the preprocessor: " << MONSTER + Monster << "\n";
     cout << "Twice the monster is: " << doubler(Monster) << "\n";
     cout << "The value of x is: " << x << "\n";
-    cout << "X * Z = " << multiplyer(x,z) << "\n";
+    cout << "X * Z = " << feature.multiplyer(x,z) << "\n";
     
     for (int i = 0; i < 100; i++){
         cout << "X is currently " << tripler(x) << "\n"; x++;
