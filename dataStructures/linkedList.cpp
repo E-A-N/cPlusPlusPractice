475bf0de5953589ac25f09418de2913dd4fc7974
Cyborg_Ean <ean.director@gmail.com>
2017-04-13 16:52:17 -0700

    practicing c++ linked list

---
+++ dataStructures/linkedList.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+struct Node {
+    int data;
+    Node* next; //reference next node via pointer
+}
+
+//Setup 1st Node only
+void nodeInit(struct Node *headNode, int num){
+    //Node 
+}
+
+int main(){
+
+
+
+    return 0;
+}
