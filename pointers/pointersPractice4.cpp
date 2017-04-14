ad0baa57d96d5e84da97abb8581364eb6e34e5a0
Cyborg_Ean <ean.director@gmail.com>
2017-04-13 17:04:14 -0700

    practicing pointers and data structures

---
+++ pointers/pointersPractice4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+struct test{
+    int val;
+    bool completed;
+};
+
+
+int main(){
+    test g; //unlike objects structs do no require 'new' syntax
+    g.val = 7777;
+    g.completed = true;
+
+
+
+
+
+    return 0;
+}
