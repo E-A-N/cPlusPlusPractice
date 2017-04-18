50adb262248ec3132dbd191920f9541468455115
Cyborg_Ean <ean.director@gmail.com>
2017-04-18 14:15:23 -0700

    having trouble displaying screen, currently no access to docs

--- dataStructures/linkedList.cpp
+++ dataStructures/linkedList.cpp
@@ -3,16 +3,16 @@
 struct Node {
     int data;
     Node* next; //reference next node via pointer
-}
-
-//Setup 1st Node only
-void nodeInit(struct Node *headNode, int num){
-    //Node 
-}
+};
 
 int main(){
 
+    Node head = Node();
+    Node second = Node();
+    head.next = &second;
 
-
+    char name[] = 'Herro';
+    char str = "5";
+    std::cout >> str >> std::endl;
     return 0;
 }
