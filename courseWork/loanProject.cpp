caca8f93fde4b5ac9a51ba2b1241f4b04a306e7c
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-04-18 20:41:06 -0700

    Update loanProject.cpp

--- courseWork/loanProject.cpp
+++ courseWork/loanProject.cpp
@@ -7,7 +7,10 @@ class Loan{
     
     public:
         Loan();
-        Loan(double,int,double);
+        Loan(double,double,int);
+        
+        double monthlyPayment;
+        double totalPayment;
         
         //setters
         void setNumberOfYears(int years){
@@ -42,12 +45,18 @@ class Loan{
         double getTotalPayment(){
             return totalPayment;
         }
-
 };
+
+Loan::Loan(double rate, double amount, int years){
+    setAnnualInterestRate(rate);
+    setLoanAmount(amount);
+    setNumberOfYears(years);
+    
+}
 
 
 int main(){
-  
+    Loan eLow = Loan(5.5,40000.0,15);
   
     return 0;
 }
