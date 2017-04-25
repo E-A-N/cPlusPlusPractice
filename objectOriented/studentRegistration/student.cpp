9dd6628156cc133adbdd91c3ea615b337c481314
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-04-25 16:20:37 -0700

    Create student.cpp

---
+++ objectOriented/studentRegistration/student.cpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "student.h"
+class student
+{
+public:
+
+	student::student() {
+		//constructor
+		name = "";
+		student_id = 0;
+	}
+
+	student::~student(){
+	}
+
+	void student::add_course(int course) {
+		course_id.push_back(course)
+	}
+};
+
