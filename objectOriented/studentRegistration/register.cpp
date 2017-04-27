2519ece0832c459fe4b76028432520ae92be9f0b
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-04-27 16:01:07 -0700

    Update register.cpp

--- objectOriented/studentRegistration/register.cpp
+++ objectOriented/studentRegistration/register.cpp
@@ -1,10 +1,58 @@
 #include "student.h"
 #include "course.h"
 
+
+void findStudentId(int id, std::vector studentList) {
+	for (int i = 0; i < studentList.size(); i++) {
+		if (id == studentList.size()) {
+			std::cout << "Enter the course id to add" << std::endl;
+		}
+	}
+}
+
+void findCourseId(int id, std::vector courseList) {
+	for (int i = 0; i < courseList.size(); i++) {
+		if (id == courseList[i]) {
+			studentList.add_course(id)
+		}
+	}
+}
+
 int main() {
 	student one("Student1", 1);
 	student two("Student2", 2);
 	course courseOne("math", 1);
 	course courseTwo("Cis", 2);
+	std::vector<student> studentList;
+	std::vector<course> courseList;
 
+	studentList.push_back(one);
+	studentList.push_back(two);
+	courseList.push_back(courseOne);
+	courseList.push_back(courseTwo);
+
+	int choice = 0;
+	int studentID;
+	int courseID;
+	while (choice != 4) {
+		std::cout << "1. to add course" << std::endl;
+		std::cout << "2. to drop" << std::endl;
+		std::cout << "3. to print" << std::endl;
+		std::cout << "4. to quit" << std::endl;
+		std::cin >> choice;
+
+		switch (choice) {
+		case 1:
+			std::cout << "Enter your student id" << std::endl;
+			std::cin >> studentID;
+			findStudentID(studentID)
+			break;
+
+		case 2:
+			break;
+
+		case 3:
+			break;
+		}
+	}
 }
