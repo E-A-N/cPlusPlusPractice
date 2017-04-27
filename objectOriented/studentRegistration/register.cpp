defe868076e61da75a7adbaa0786a95174bfd3da
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-04-27 15:36:04 -0700

    Create register.cpp

---
+++ objectOriented/studentRegistration/register.cpp
@@ -0,0 +1,10 @@
+#include "student.h"
+#include "course.h"
+
+int main() {
+	student one("Student1", 1);
+	student two("Student2", 2);
+	course courseOne("math", 1);
+	course courseTwo("Cis", 2);
+
+}
