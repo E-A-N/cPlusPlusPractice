859eac44d845591c53caac2d0acef0e71dd4a343
Cyborg_Ean <ean.director@gmail.com>
2017-05-16 20:07:58 -0700

    completed prototype of person project

--- objectOriented/personClass/Main.cpp
+++ objectOriented/personClass/Main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
+#include <string>
 #include "Person.h"
 #include "Student.h"
 #include "Worker.h"
+using namespace std;
+class StudentWorkFactory {
+    public:
+        static StudentWorker *newStudent(const string &school, const string &job){
+            return new StudentWorker(school,job);
+        }
+};
 
-int main(){
+int main() {
 
     //Worker* Ean = new Worker();
-    Worker Ean;
+    //Worker Ean;
+
+    int size = 5;
+    string school;
+    string job;
+
+    StudentWorker workingStudents[size];
+    for(int i = 0; i < size; i++){
+        cout << "What\'s the name of the school?: ";
+        cin >> school;
+
+        cout << "\nWhat\'s the name of the job?: ";
+        cin >> job;
+
+        workingStudents[i] = *newStudent(school, job);
+    }
 
     return 0;
 }
