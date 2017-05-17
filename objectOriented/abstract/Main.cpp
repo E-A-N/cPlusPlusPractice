9ab0a2f0b1a220b8c278ca792cdba515181b0ae0
Cyborg_Ean <ean.director@gmail.com>
2017-05-16 23:28:24 -0700

    added vector of shape elements

--- objectOriented/abstract/Main.cpp
+++ objectOriented/abstract/Main.cpp
@@ -1,11 +1,23 @@
 #include "Shape.h"
 #include "Rectangle.h"
 #include "Circle.h"
+#include <vector>
 using namespace std;
 
 int main(){
 
       vector<Shape*>  shape_array;
-  
+      Circle c1 = new Circle();
+      Rectangle r1 = new Rectangle();
+
+      shape_array.push_back(c1);
+      shape_array.push_back(r1);
+      shape_array[0].print();
+      shape_array[1].print();
+
+      delete shape_array[0];
+      delete shape_array[1];
+
+
       return 0;
 }
